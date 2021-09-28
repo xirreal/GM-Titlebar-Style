dfa7251baaf8dbc65b3d3abde434218eb401a9f8
alethéia <34745171+xirreal@users.noreply.github.com>
2021-09-28 17:24:34 +0200

    Initial commit

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
+import * as settings from '@goosemod/settings';
 
+const settingsStore = { color: "#f2f2f2" };
+
+const meta = document.createElement("meta");
+      meta.name = "theme-color";
+      meta.content = settingsStore.color;
+
+export default {
+    goosemodHandlers: {
+        getSettings: () => [settingsStore],
+        loadSettings: ([_loadedSettings]) => {
+            settingsStore.color = _loadedSettings.color
+        },
+
+        onImport: async () => {
+            document.getElementsByTagName("head")[0].prepend(meta);
+
+            createItem('Titlebar Style', ['',
+                {
+                    type: 'text-and-color',
+                    text: 'Titlebar color',
+                    initialValue: () => settingsStore.color,
+                    oninput: (value) => {
+                        settingsStore.color = value;
+                    },
+                },
+            ]);
+        },
+
+        onRemove: () => {
+            meta.remove();
+            removeItem('Titlebar Style');
+        },
+    }
+};
